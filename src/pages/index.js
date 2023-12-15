21a070b2930284c0aff6d8b88309e47493bcc23d
ruhatb <r.uhat@yahoo.com.tr>
2023-12-15 18:12:22 +0300

    created pages.

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+export { default as About } from "./About";
+export { default as Home } from "./Home";
+export { default as Shop } from "./Shop";
+export { default as Blog } from "./Blog";
+export { default as Pages } from "./Pages";
+export { default as Contact } from "./Contact";
+export  {default as Login } from "./Login";
+export  {default as Register } from "./Register";
+
+
